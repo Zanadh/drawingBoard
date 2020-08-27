0252c4d8c933060c5c2167e4801f0974fa09efcc
M Fauzan Adhim <fadzim018@gmail.com>
2020-08-27 13:52:17 +0700

    adding base layout

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+
+var preloader = document.getElementById('loader');
+ 
+
+document.addEventListener("DOMContentLoaded", function() { 
+    preloader.style.display = 'none';
+});
+//   alert("This alert box was called with the onload event");
